43350e4e0379cbf3a045644e1e472d0ce5531774
Sarasi <sarasividumini13@gmail.com>
2026-03-17 10:45:37 +0530

    Created exception package . It includes CustomException and Global Handler Exception. And Also Created APIResponse util class

---
+++ TriGlow-GlowCare_Backend/src/main/java/lk/ijse/triglowglowcare_backend/util/APIResponse.java
@@ -0,0 +1,17 @@
+package lk.ijse.triglowglowcare_backend.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class APIResponse<T> {
+
+    private int status;
+    private String message;
+    private T data;
+
+}
