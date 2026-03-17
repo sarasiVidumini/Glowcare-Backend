8d981d22cb4dacce6a382a64ae677a5cd42c3d34
Sarasi <sarasividumini13@gmail.com>
2026-03-17 09:51:46 +0530

    Updated Routine Repo

--- TriGlow-GlowCare_Backend/src/main/java/lk/ijse/triglowglowcare_backend/repository/RoutineRepo.java
+++ TriGlow-GlowCare_Backend/src/main/java/lk/ijse/triglowglowcare_backend/repository/RoutineRepo.java
@@ -1,6 +1,7 @@
 package lk.ijse.triglowglowcare_backend.repository;
 
+import lk.ijse.triglowglowcare_backend.entity.Routine;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoutineRepo extends JpaRepository<RoutineRepo, Long> {
+public interface RoutineRepo extends JpaRepository<Routine, Long> {
 }
